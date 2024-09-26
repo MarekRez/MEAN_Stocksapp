09c69325d4e398482962da8b44c4c060655f2ba3
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2024-09-26 17:54:37 +0200

    Last changes before lecture 1

---
+++ lecture.js
@@ -0,0 +1,8 @@
+/**************************************************************************************
+ *                                                                                    *
+ *        Please do not write anything in this file.                                  *
+ *        This file is for lecture purposes only. All changes will be ignored.        *
+ *                                                                                    *
+ **************************************************************************************/
+
+console.log('Hello world!');
