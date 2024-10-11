d2d1e47cb1e66c8782cbb60644a310015e24ddbb
marek-rezny <rezny.marek@gmail.com>
2024-10-11 14:11:09 +0200

    Lekcia 2 koniec

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,21 @@ const meno: Person = {
     role:Role.ADMIN
 };
 console.log(`Hello ${meno.firstName} ${meno.lastName} ${meno.role}!`);
+
+class Stock {
+    public stockName: string;
+    private marketOpening: string;
+    private marketClosing: string;
+    public stockPrice: number;
+
+    constructor(stockName:string, marketOpening: string, marketClosing: string, stockPrice: number) {
+        this.stockName = stockName;
+        this.marketOpening = marketOpening;
+        this.marketClosing = marketClosing;
+        this.stockPrice = stockPrice;
+    }
+    public getPrice():number{
+        return this.stockPrice;
+    }
+}
+const stock = new Stock("Apple", "Tuesday", "Friday", 250);
