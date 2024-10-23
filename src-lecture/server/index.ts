623ffaff1965c306c4da4f2d5515dbb626cea588
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2024-10-23 16:55:38 +0200

    express '/'

--- src-lecture/server/index.ts
+++ src-lecture/server/index.ts
@@ -5,10 +5,10 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/api', (req: Request, res: Response) => {
-    res.json({message: 'Hello, this is your Express.js REST API!'});
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, this is your Express.js REST API!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}/`);
 });
